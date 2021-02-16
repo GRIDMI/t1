296fe6b3632c1dea40afefdec8110203a0b693d5
GRIDMI <51048368+GRIDMI@users.noreply.github.com>
2021-02-16 11:32:26 +0300

    from a30

--- app/src/main/java/com/gridmi/myapplication/MainActivity.java
+++ app/src/main/java/com/gridmi/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        a("a", 27);
+        a("a", 30);
     }
 
     private void a(String t, int c) {
